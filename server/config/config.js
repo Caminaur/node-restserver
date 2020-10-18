b656de6cb679e2fabdd2263306353ba28270355c
Caminaur <jacaminaur@hotmail.com>
2020-10-18 02:37:34 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb+srv://das_admin_user:mEWBKsc5HBykVnMJ@cluster0.noapj.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
